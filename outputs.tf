bce372eb1aac1264d9a00421f2c290cacebd59bb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "sentinel_data_connector_microsoft_threat_intelligences" {
-  description = "All sentinel_data_connector_microsoft_threat_intelligence resources"
-  value       = azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences
-}
 output "sentinel_data_connector_microsoft_threat_intelligences_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_intelligences"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_microsoft_emerging_threat_feed_lookback_date" {
-  description = "List of microsoft_emerging_threat_feed_lookback_date values across all sentinel_data_connector_microsoft_threat_intelligences"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : v.microsoft_emerging_threat_feed_lookback_date]
+  description = "Map of microsoft_emerging_threat_feed_lookback_date values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.microsoft_emerging_threat_feed_lookback_date }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_name" {
-  description = "List of name values across all sentinel_data_connector_microsoft_threat_intelligences"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : v.name]
+  description = "Map of name values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.name }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_microsoft_threat_intelligences"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.tenant_id }
 }
 
