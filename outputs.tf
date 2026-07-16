3cd87f23b909f637cf8fd6e4117fa8538f0f61e0
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "sentinel_data_connector_microsoft_threat_intelligences_id" {
   description = "Map of id values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
-  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.id }
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
-  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.log_analytics_workspace_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.log_analytics_workspace_id if v.log_analytics_workspace_id != null && length(v.log_analytics_workspace_id) > 0 }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_microsoft_emerging_threat_feed_lookback_date" {
   description = "Map of microsoft_emerging_threat_feed_lookback_date values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
-  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.microsoft_emerging_threat_feed_lookback_date }
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.microsoft_emerging_threat_feed_lookback_date if v.microsoft_emerging_threat_feed_lookback_date != null && length(v.microsoft_emerging_threat_feed_lookback_date) > 0 }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_name" {
   description = "Map of name values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
-  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.name }
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "sentinel_data_connector_microsoft_threat_intelligences_tenant_id" {
   description = "Map of tenant_id values across all sentinel_data_connector_microsoft_threat_intelligences, keyed the same as var.sentinel_data_connector_microsoft_threat_intelligences"
-  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.tenant_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_intelligence.sentinel_data_connector_microsoft_threat_intelligences : k => v.tenant_id if v.tenant_id != null && length(v.tenant_id) > 0 }
 }
 
